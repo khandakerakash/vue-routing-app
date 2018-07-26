c8db5f1cc1794fa5ce8a3516d5b9a95fa31201c5
Akash <akash@tgalimited.com>
2018-07-26 17:16:36 +0600

    A little bit modified the whole layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-    <hr>
+    <div class="container">
+      <h1>{{ msg }}</h1>
+      <hr>
 
-    <div class="navigation">
-      <ul class="nav nav-pills nav-fill">
-        <li class="nav-item">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/about">About</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/contact">Contact</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/blog">Blog</router-link>
-        </li>
-      </ul>
+      <div class="navigation">
+        <ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact">Contact</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/blog">Blog</router-link>
+          </li>
+        </ul>
+      </div>
+      <router-view></router-view>
     </div>
-
-    <router-view></router-view>
   </div>
 </template>
 
@@ -41,13 +42,30 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #f2f2f2;
-  color: #2c3e50;
-  width: 50rem;
-  height: 50vh;
-  margin: 10rem auto !important;
-  padding: 2rem;
 }
+
+.container {
+    padding-top: 1.5rem;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  @media (min-width: 768px) {
+    .container {
+      width: 750px;
+    }
+  }
+  @media (min-width: 992px) {
+    .container {
+      width: 970px;
+    }
+  }
+  @media (min-width: 1200px) {
+    .container {
+      width: 1170px;
+    }
+  }
 
 h1, h2 {
   font-weight: normal;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from "./routes"
 
+
+
+
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
